996bd86592b8e2bf29b239395081b04ba7929702
pgnickb <pgnickb@github.com>
2023-02-11 20:07:54 +0100

    pgindent run

--- uuid_v1_ops.c
+++ uuid_v1_ops.c
@@ -1,29 +1,31 @@
 #include <uuid_v1_ops.h>
 
-Datum 
+Datum
 is_uuid_v1(PG_FUNCTION_ARGS)
 {
-    pg_uuid_t  *arg1 = PG_GETARG_UUID_P(0);
-    PG_RETURN_BOOL(
-        (arg1->data[UUID_VERSION_OFFSET] & 0xf0) == 0x10
-        );
+	pg_uuid_t  *arg1 = PG_GETARG_UUID_P(0);
+
+	PG_RETURN_BOOL(
+				   (arg1->data[UUID_VERSION_OFFSET] & 0xf0) == 0x10
+		);
 }
 
-Datum 
+Datum
 uuid_v1_to_timestamptz(PG_FUNCTION_ARGS)
 {
-    pg_uuid_t  *arg1 = PG_GETARG_UUID_P(0);
-    uint64 res = 0;
-    uint64 gns; /* number of 100ns intervals since EPOCH */
-    gns = ((uint64) ((arg1->data[order[0]]) & 0x0F)) << 56;
-    for (int i=1; i<UUID_V1_TIMESTAMP_LEN; ++i)
-    {
-        /* this conversion is safe regardless of the endiannes */
-        gns += ((uint64) arg1->data[order[i]]) << ((UUID_V1_TIMESTAMP_LEN-i-1) * 8);
-    }
+	pg_uuid_t  *arg1 = PG_GETARG_UUID_P(0);
+	uint64		res = 0;
+	uint64		gns;			/* number of 100ns intervals since EPOCH */
 
-    res = (gns/UUID_V1_100NS_TO_USEC)-GREGORIAN_BEGINNING_OFFSET_USEC;
-    PG_RETURN_TIMESTAMPTZ(res);
+	gns = ((uint64) ((arg1->data[order[0]]) & 0x0F)) << 56;
+	for (int i = 1; i < UUID_V1_TIMESTAMP_LEN; ++i)
+	{
+		/* this conversion is safe regardless of the endiannes */
+		gns += ((uint64) arg1->data[order[i]]) << ((UUID_V1_TIMESTAMP_LEN - i - 1) * 8);
+	}
+
+	res = (gns / UUID_V1_100NS_TO_USEC) - GREGORIAN_BEGINNING_OFFSET_USEC;
+	PG_RETURN_TIMESTAMPTZ(res);
 }
 
 Datum
@@ -38,14 +40,14 @@ uuid_v1_cmp(PG_FUNCTION_ARGS)
 static int
 uuid_v1_internal_cmp(const pg_uuid_t *arg1, const pg_uuid_t *arg2)
 {
-    int res;
+	int			res;
 
-    for (int i=0;i<UUID_LEN;++i)
-    {
-        res = arg1->data[order[i]] - arg2->data[order[i]];
-        if (res != 0)
-            break;
-    }
+	for (int i = 0; i < UUID_LEN; ++i)
+	{
+		res = arg1->data[order[i]] - arg2->data[order[i]];
+		if (res != 0)
+			break;
+	}
 
 	return res;
 }
--- uuid_v1_ops.h
+++ uuid_v1_ops.h
@@ -16,30 +16,34 @@
 PG_MODULE_MAGIC;
 
 PG_FUNCTION_INFO_V1(is_uuid_v1);
-Datum is_uuid_v1(PG_FUNCTION_ARGS);
+Datum		is_uuid_v1(PG_FUNCTION_ARGS);
 
 PG_FUNCTION_INFO_V1(uuid_v1_cmp);
-Datum uuid_v1_cmp(PG_FUNCTION_ARGS);
+Datum		uuid_v1_cmp(PG_FUNCTION_ARGS);
 
 PG_FUNCTION_INFO_V1(uuid_v1_internalize);
-Datum uuid_v1_internalize(PG_FUNCTION_ARGS);
+Datum		uuid_v1_internalize(PG_FUNCTION_ARGS);
 
 PG_FUNCTION_INFO_V1(uuid_v1_to_timestamptz);
-Datum uuid_v1_to_timestamptz(PG_FUNCTION_ARGS);
+Datum		uuid_v1_to_timestamptz(PG_FUNCTION_ARGS);
 
 PG_FUNCTION_INFO_V1(uuid_v1_lt);
-Datum uuid_v1_lt(PG_FUNCTION_ARGS);
+Datum		uuid_v1_lt(PG_FUNCTION_ARGS);
+
 PG_FUNCTION_INFO_V1(uuid_v1_le);
-Datum uuid_v1_le(PG_FUNCTION_ARGS);
+Datum		uuid_v1_le(PG_FUNCTION_ARGS);
+
 PG_FUNCTION_INFO_V1(uuid_v1_eq);
-Datum uuid_v1_eq(PG_FUNCTION_ARGS);
+Datum		uuid_v1_eq(PG_FUNCTION_ARGS);
+
 PG_FUNCTION_INFO_V1(uuid_v1_ge);
-Datum uuid_v1_ge(PG_FUNCTION_ARGS);
+Datum		uuid_v1_ge(PG_FUNCTION_ARGS);
+
 PG_FUNCTION_INFO_V1(uuid_v1_gt);
-Datum uuid_v1_gt(PG_FUNCTION_ARGS);
+Datum		uuid_v1_gt(PG_FUNCTION_ARGS);
 
-static int uuid_v1_internal_cmp(const pg_uuid_t *arg1, const pg_uuid_t *arg2);
+static int	uuid_v1_internal_cmp(const pg_uuid_t *arg1, const pg_uuid_t *arg2);
 
-static unsigned int order[UUID_LEN] = {6,7,4,5,0,1,2,3,8,9,10,11,12,13,14,15};
+static unsigned int order[UUID_LEN] = {6, 7, 4, 5, 0, 1, 2, 3, 8, 9, 10, 11, 12, 13, 14, 15};
 
-#endif /* UUID_V1_OPS_H */
+#endif							/* UUID_V1_OPS_H */
