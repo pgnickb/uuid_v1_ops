5f918c0dc6b889186ef280697075114403bf87c2
Nick Babadzhanian <33933459+pgnickb@users.noreply.github.com>
2023-02-11 18:52:19 +0100

    Address #7
    - Added a function to check if UUID is in fact v1
    - Removed an artifact _internalize function
    - Added various null-value tests
    - Fixed function signatures

--- sql/uuid_v1_ops.sql
+++ sql/uuid_v1_ops.sql
@@ -9,11 +9,36 @@ create extension uuid_v1_ops;
 /* seed random for consistent dataset */
 select setseed(0.2);
 
-select plan(6);
-/* tests */
-select is(uuid_v1_cmp(uuid'00000000-0000-0000-0000-000000000000', uuid'bc2cd750-a63b-11ed-84a2-123456789112') < 0, true, 'Less then works fine');
-select is(uuid_v1_cmp(uuid'bc2cd750-a63b-11ed-84a2-123456789112', uuid'00000000-0000-0000-0000-000000000000') > 0, true, 'Greater then works fine');
-select is(uuid_v1_cmp(uuid'00000000-0000-0000-0000-000000000000', uuid'00000000-0000-0000-0000-000000000000') = 0, true, 'Equals works fine');
+select plan(15);
+/* tests: */
+
+/* comparison */
+
+select is(
+            uuid_v1_cmp(uuid'00000000-0000-0000-0000-000000000000',
+                        null::uuid
+                        ), 
+            null, 
+            'uuid_v1_cmp handles nulls properly');
+
+select is(
+            uuid_v1_cmp(uuid'00000000-0000-0000-0000-000000000000',
+                        uuid'bc2cd750-a63b-11ed-84a2-123456789112'
+                        ) < 0, 
+            true, 
+            'Less then works fine');
+select is(
+            uuid_v1_cmp(uuid'bc2cd750-a63b-11ed-84a2-123456789112', 
+                        uuid'00000000-0000-0000-0000-000000000000'
+                        ) > 0, 
+            true, 
+            'Greater then works fine');
+select is(
+            uuid_v1_cmp(uuid'00000000-0000-0000-0000-000000000000', 
+                        uuid'00000000-0000-0000-0000-000000000000'
+                      ) = 0, 
+            true, 
+            'Equals works fine');
 select is(
             uuid_v1_to_timestamptz(uuid'00000000-0000-0000-0000-000000000000') at time zone 'UTC', 
             timestamp'1582-10-15 00:00:00', 
@@ -26,6 +51,31 @@ select is(
             uuid_v1_to_timestamptz(uuid'bc2cd750-a63b-11ed-84a2-123456789112') at time zone 'UTC', 
             timestamp'2023-02-06 16:31:40.869', 
             'Some random timestamp');
+
+/* is_uuid works fine */
+
+select is(
+            is_uuid_v1(uuid'00000000-0000-1000-0000-000000000000'),
+            true,
+            'This UUID is correctly recognized as v1');
+
+select is(
+            is_uuid_v1(uuid'00000000-0000-2000-0000-000000000000'),
+            false,
+            'This UUID is correctly recognized as non-v1');
+
+select is(
+            is_uuid_v1(null::uuid),
+            null,
+            'This UUID is correctly recognized as non-v1');
+
+/* tests with various operators and their interactions with nulls */
+
+select is(uuid'00000000-0000-2000-0000-000000000000' ~<  null, null,  '~< handles null properly');
+select is(uuid'00000000-0000-2000-0000-000000000000' ~<= null, null, '~<= handles null properly');
+select is(uuid'00000000-0000-2000-0000-000000000000' =   null, null,   '= handles null properly');
+select is(uuid'00000000-0000-2000-0000-000000000000' ~>= null, null, '~>= handles null properly');
+select is(uuid'00000000-0000-2000-0000-000000000000' ~>  null, null,  '~> handles null properly');
 
 
 select * from finish();
