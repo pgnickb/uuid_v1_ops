ea23a85bb683234685e72b70ae736a78113804c5
pgnickb <pgnickb@github.com>
2023-02-12 08:58:17 +0100

    Finalize

--- uuid_v1_ops.c
+++ uuid_v1_ops.c
@@ -3,7 +3,7 @@
 Datum
 is_uuid_v1(PG_FUNCTION_ARGS)
 {
-    pg_uuid_t  *arg1 = PG_GETARG_UUID_P(0);
+	pg_uuid_t  *arg1 = PG_GETARG_UUID_P(0);
 
 	PG_RETURN_BOOL(
 				   (arg1->data[UUID_VERSION_OFFSET] & 0xf0) == 0x10
